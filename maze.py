c14f9ee22c4e75d0deaf0e04011c2f6f92551f1f
knalavadi <krisha.nalavadi@gmail.com>
2015-10-05 15:29:21 -0700

    updated maze

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 import turtle 
 
+#starting coordinates of maze lines
 FIRST_LINE_HORIZ = (-450,-350)  
 SECOND_LINE_HORIZ= (-450,-300) 
 THIRD_LINE_VERT = (-350,-150)  
@@ -19,17 +20,27 @@ SIXTEENTH_LINE_VERT = (75,250)
 SEVENTEENTH_LINE_HORIZ = (25,-50)
 EIGHTEENTH_LINE_HORIZ = (75,-100)
 NINETEENTH_LINE_VERT = (-125,-50)
+TWENTIETH_LINE_VERT = (-75,-100)
+TWENTIFIRST_LINE_HORIZ = (-75,-150)
+TWENTISECOND_LINE_HORIZ = (-125,-200)
+TWENTITHIRD_LINE_VERT = (25,-200)
+TWENTIFOURTH_LINE_VERT = (75,-150)
+TWENTIFIFTH_LINE_HORIZ = (75,-300)
+TWENTISIXTH_LINE_HORIZ = (25,-250)
+
 
 turtle.setup(1000,1000)	# set the window size to 1000 by 1000 pixels
 wn = turtle.Screen()
 krisha = turtle.Turtle()
 
+#pen color and size
+krisha.pensize(3.5)
+krisha.pencolor("#00afff") 
 
-krisha.pensize(3)
-krisha.pencolor("#00bfff")
 
 krisha.penup()
 
+#draws maze trajectory 
 krisha.setposition(FIRST_LINE_HORIZ)
 krisha.pendown()
 krisha.forward(150)
@@ -46,6 +57,8 @@ krisha.right(90)
 krisha.forward(150)
 krisha.penup()
 
+krisha.pencolor("#00cfff") 
+
 krisha.setposition(FOURTH_LINE_VERT)
 krisha.pendown()
 krisha.forward(250)
@@ -57,6 +70,7 @@ krisha.right(90)
 krisha.forward(75)
 krisha.penup()
 
+
 krisha.setposition(SIXTH_LINE_HORIZ)
 krisha.pendown()
 krisha.forward(75)
@@ -67,6 +81,9 @@ krisha.pendown()
 krisha.right(90)
 krisha.forward(200)
 krisha.penup()
+
+
+krisha.pencolor("#00efff") 
 
 krisha.setposition(EIGTH_LINE_VERT)
 krisha.pendown()
@@ -90,6 +107,9 @@ krisha.left(90)
 krisha.forward(200)
 krisha.penup()
 
+
+krisha.pencolor("#00ccdd") 
+
 krisha.setposition(TWELVETH_LINE_VERT)
 krisha.pendown()
 krisha.forward(200)
@@ -112,10 +132,13 @@ krisha.right(90)
 krisha.forward(250)
 krisha.penup()
 
+
 krisha.setposition(SIXTEENTH_LINE_VERT)
 krisha.pendown()
 krisha.forward(350)
 krisha.penup()
+
+krisha.pencolor("#00ccbb") 
 
 krisha.setposition(SEVENTEENTH_LINE_HORIZ)
 krisha.pendown()
@@ -133,5 +156,54 @@ krisha.pendown()
 krisha.left(90)
 krisha.forward(150)
 krisha.penup()
+
+krisha.setposition(TWENTIETH_LINE_VERT)
+krisha.pendown()
+krisha.forward(50)
+krisha.penup()
+
+krisha.pencolor("#00eeaa") 
+
+krisha.setposition(TWENTIFIRST_LINE_HORIZ)
+krisha.pendown()
+krisha.left(90)
+krisha.forward(150)
+krisha.penup()
+
+krisha.setposition(TWENTISECOND_LINE_HORIZ)
+krisha.pendown()
+krisha.forward(150)
+krisha.penup()
+
+krisha.setposition(TWENTITHIRD_LINE_VERT)
+krisha.pendown()
+krisha.right(90)
+krisha.forward(50)
+krisha.penup()
+
+krisha.setposition(TWENTIFOURTH_LINE_VERT)
+krisha.pendown()
+krisha.forward(150)
+krisha.penup()
+
+krisha.setposition(TWENTIFIFTH_LINE_HORIZ)
+krisha.pendown()
+krisha.right(90)
+krisha.forward(150)
+krisha.penup()
+
+krisha.setposition(TWENTISIXTH_LINE_HORIZ)
+krisha.pendown()
+# krisha.right(90)
+krisha.forward(150)
+krisha.penup()
+
+
+#krisha.pencolor("#00ffaa") 
+
+
+
+
+# krisha.pencolor("#00ff00") # last pencolor
 
 turtle.mainloop() #keeps turtle open until user exits 
